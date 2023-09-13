07a63c29005650f89e3f83e3790e934223f9e7b4
truongvan <truongvan@live.com>
2023-09-13 08:12:12 +0700

    Style: Replace Tailwind class `rounded-sm` with `rounded`.

--- src/lib/components/sidebar/Refresh.svelte
+++ src/lib/components/sidebar/Refresh.svelte
@@ -26,6 +26,6 @@
 <button
     on:click|stopPropagation={handleClick}
     on:mousedown|stopPropagation
-    class="w-6 h-6 hover:bg-magnum-400 active:bg-magnum-600 rounded-sm hover:text-white">
+    class="w-6 h-6 hover:bg-magnum-400 active:bg-magnum-600 rounded hover:text-white">
     <i class="bi bi-arrow-clockwise" />
 </button>
