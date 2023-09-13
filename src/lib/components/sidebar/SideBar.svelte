36c964e529df8a4fe752244bc83c7fec2570b299
truongvan <truongvan@live.com>
2023-09-13 08:06:33 +0700

    Feat: Add toggle button for sidebar visibility

--- src/lib/components/sidebar/SideBar.svelte
+++ src/lib/components/sidebar/SideBar.svelte
@@ -2,11 +2,12 @@
     import { onMount } from "svelte";
     import Accordion from "./Accordion.svelte";
     import SideBarHeader from "./SideBarHeader.svelte";
+    import { sidebarWidthStore } from "$lib/channel/store";
 
     const MAX_WIDTH = 500;
     let sidebarElement: HTMLElement;
-    let minWidth = 150;
-    let sidebarWidth = "fit-containt";
+    let minWidth = 170;
+    $sidebarWidthStore.width = "fit-containt";
     let sidebarMinWidth = `${minWidth}px`;
 
     onMount(async () => {
@@ -23,15 +24,14 @@
             const wrapperElement = sidebarElement.parentElement;
             if (wrapperElement) {
                 if (x < minWidth / 2) {
-                    sidebarWidth = `0px`;
+                    $sidebarWidthStore.width = `0px`;
                     sidebarMinWidth = `0px`;
                 } else {
-                    sidebarWidth = `${Math.min(x, MAX_WIDTH)}px`;
+                    $sidebarWidthStore.width = `${Math.min(x, MAX_WIDTH)}px`;
                     sidebarMinWidth = `${minWidth}px`;
                 }
             }
         }
-
         const onMouseMove = (e: MouseEvent) => {
             moveAt(e.pageX);
         };
@@ -47,23 +47,26 @@
     }
 </script>
 
-<div
-    aria-hidden={sidebarWidth == "0px"}
-    style:width={sidebarWidth}
-    style:min-width={sidebarMinWidth}
-    bind:this={sidebarElement}
-    aria-label="Docs navigation"
-    class="flex group flex-col shrink-0 text-white bg-magnum-500 sidebar relative">
+{#if $sidebarWidthStore.show}
     <div
-        role="button"
-        tabindex="0"
-        on:mousedown|preventDefault={handleResizeBarDrag}
-        on:dragstart={() => false}
-        class="group-aria-hidden:translate-x-0 w-1 h-full -translate-x-1/2 transition-[background-color] hover:cursor-ew-resize hover:bg-magnum-300 reside-bar absolute left-full top-0" />
-    <div class="px-3">
-        <SideBarHeader />
-        <div class="overflow-hidden">
-            <Accordion />
+        aria-hidden={$sidebarWidthStore.width == "0px"}
+        style:width={$sidebarWidthStore.width}
+        style:min-width={sidebarMinWidth}
+        bind:this={sidebarElement}
+        aria-label="Docs navigation"
+        class="flex group flex-col shrink-0 text-black bg-gray-100 sidebar relative">
+        <div
+            role="button"
+            tabindex="0"
+            on:mousedown|preventDefault={handleResizeBarDrag}
+            on:dragstart={() => false}
+            class="group-aria-hidden:translate-x-0 w-1 h-full -translate-x-1/2 transition-[background-color] hover:cursor-ew-resize hover:bg-magnum-300 reside-bar absolute left-full top-0" />
+        <div class="px-3">
+            <SideBarHeader />
+
+            <div class="overflow-hidden">
+                <Accordion />
+            </div>
         </div>
     </div>
-</div>
+{/if}
---
+++ src/lib/components/sidebar/ToggleSidebar.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { sidebarWidthStore } from "$lib/channel/store";
+
+    function handleClick() {
+        $sidebarWidthStore.show = !$sidebarWidthStore.show;
+    }
+</script>
+
+<button
+    aria-checked={$sidebarWidthStore.show}
+    on:click|stopPropagation={handleClick}
+    on:mousedown|stopPropagation
+    class="w-6 h-6 aria-checked:bg-magnum-500 aria-checked:text-white hover:bg-magnum-400 active:bg-magnum-600 rounded hover:text-white">
+    <i class="bi bi-layout-sidebar" />
+</button>
