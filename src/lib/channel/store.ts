7af5596a19fc5b16fc85a4ae55960dd2aa1dedb8
truongvan <truongvan@live.com>
2023-09-13 08:30:25 +0700

    Feat: Add store for ToggleSidebar

--- src/lib/channel/store.ts
+++ src/lib/channel/store.ts
@@ -1,12 +1,11 @@
 import type { Channel } from "./type";
 
 import { writable } from "svelte/store";
-import { getAllChannel, updateOrCreateChannel } from "$lib/query/channel";
+import { getAllChannel } from "$lib/query/channel";
 
 export function create_db_store(initalValue: Channel[] = []) {
     const { set, update, subscribe } = writable(initalValue);
     let unsubscribe = function () {};
-    let updateTimeout: number;
     return {
         set,
         update,
@@ -23,3 +22,7 @@ export function create_db_store(initalValue: Channel[] = []) {
 
 export const channelStore = create_db_store();
 export const selectedChannelStore = writable<Channel | null>(null);
+export const sidebarWidthStore = writable({
+    show: true,
+    width: "",
+});
